738dbade7a65018cf3f01df598fbddf4637d65c0
michaelp2002 <85465378+michaelp2002@users.noreply.github.com>
2024-01-26 21:06:55 -0500

    Add files via upload

---
+++ web-projects/test-site/scripts/main.js
@@ -0,0 +1,27 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/liked-songs.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else { 
+    myImage.setAttribute("src", "images/liked-songs.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
+
+  myButton.onclick = () => {
+    setUserName();
+  };
+    
