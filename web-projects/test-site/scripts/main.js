bb3b64f1877862662672151b516627b39a620c3f
michaelp2002 <85465378+michaelp2002@users.noreply.github.com>
2024-01-26 21:25:43 -0500

    Add files via upload

--- web-projects/test-site/scripts/main.js
+++ web-projects/test-site/scripts/main.js
@@ -9,14 +9,14 @@ myImage.onclick = () => {
   }
 };
 let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+let myHeading = document.querySelector("h1");$
 function setUserName() {
     const myName = prompt("Please enter your name.");
     if (!myName) {
       setUserName();
     } else {
       localStorage.setItem("name", myName);
-      myHeading.textContent = `Mozilla is cool, ${myName}`;
+      myHeading.textContent = `Welcome back, ${myName}`;
     }
   }
   
